bb99a46ae22f9a2f195fc3ec05f0390ee780ae21
Kurtyyy597 <kurtmarquez238@gmail.com>
2025-04-24 15:42:59 +0800

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';  // Correct import path
+import ForgotPassword from './components/ForgotPassword';  // Correct import path
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
